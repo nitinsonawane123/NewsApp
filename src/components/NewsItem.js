74fedd9f863de2b637b644333e0d2c4b19ecce5b
Nitin Sonawane <nitin.sonawane@waiin.com>
2024-05-24 15:08:53 +0530

    Video 24 Completed

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -10,7 +10,7 @@ export class NewsItem extends Component {
            <div className="card-body">
            <h5 className="card-title">{title}</h5>
            <p className="card-text">{description}</p>
-           <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+           <a href="/newsdetail" className="btn btn-primary"> Go somewhere</a>
           </div>
       </div>
       </div>
