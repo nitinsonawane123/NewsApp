ab8a2b2d37a4c9653e3506567df5d12b74654c13
Nitin Sonawane <nitin.sonawane@waiin.com>
2024-05-27 14:11:25 +0530

    Video 27 Completed

--- src/components/News.js
+++ src/components/News.js
@@ -2,20 +2,34 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+  constructor(){
+    super();
+    this.state = {
+      articles : [],
+      loading: false,
+      page:1
+    }
+  }
+
+  async componentDidMount(){
+    console.log("cdm");
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=eeefdb8907484a7fbe2ec54774a63b10&page=1";
+    let data = await fetch(url);
+    let parseData = await data.json()
+    console.log(parseData);
+    this.setState({articles: parseData.articles})
+  }
+
   render() {
     return (
       <div className="container my-3">
-        <h2>NewsMonkey - Top Headlines</h2>
+        <h1>NewsMonkey - Top Headlines</h1>
         <div className="row">
-          <div className="col-md-4">
-          <NewsItem title="myTitle" description="mydesc"/>
-        </div>
-        <div className="col-md-4">
-          <NewsItem title="myTitle" description="mydesc"/>
-        </div>
-        <div className="col-md-4">
-          <NewsItem title="myTitle" description="mydesc"/>
-        </div>
+        {this.state.articles.map((element)=>{
+            return <div className="col-md-4" key={element.url}>
+            <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+          </div>
+        })} 
         </div>
       </div>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description} = this.props;
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
-      <div>
-          <div className="card" style={{width: "18rem"}}>
-           <img src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg" className="card-img-top" alt="..."/>
+      <div className='my-3'>
+          <div className="card" style={{width: "24rem"}}>
+           <img src={!imageUrl?"https://www.livemint.com/lm-img/img/2024/05/26/1600x900/Cyclone_Remal_1716685044781_1716685045021.jpg":imageUrl} className="card-img-top" alt="..."/>
            <div className="card-body">
            <h5 className="card-title">{title}</h5>
            <p className="card-text">{description}</p>
-           <a href="/newsdetail" className="btn btn-primary"> Go somewhere</a>
+           <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
           </div>
       </div>
       </div>
