64c0eb4084338e56832e6d3b4fdda8ccd1173ae9
Nitin Sonawane <nitin.sonawane@waiin.com>
2024-05-28 16:28:50 +0530

    Video 34 Completed

--- src/components/News.js
+++ src/components/News.js
@@ -17,13 +17,18 @@ export class News extends Component {
     category: PropTypes.string,
   }
 
-  constructor(){
-    super();
+   capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  constructor(props){
+    super(props);
     this.state = {
       articles : [],
       loading: false,
       page:1
     }
+    document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`;
   }
 
   async updateNews(){
@@ -55,7 +60,7 @@ export class News extends Component {
   render() {
     return (
       <div className="container my-3">
-        <h1 className="text-center" style={{margin: '25px 0px'}}>NewsMonkey - Top Headlines</h1>
+        <h2 className="text-center" style={{margin: '25px 0px'}}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h2>
         {this.state.loading && <Spinner/>}
         <div className="row">
         {!this.state.loading && this.state.articles.map((element)=>{
