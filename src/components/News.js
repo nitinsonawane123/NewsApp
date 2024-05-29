4d6fb4a98b6088c4246b630af3ca2d24b2386252
Nitin Sonawane <nitin.sonawane@waiin.com>
2024-05-29 12:11:30 +0530

    Video 37 Completed

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
 
 
 export class News extends Component {
@@ -25,45 +26,58 @@ export class News extends Component {
     super(props);
     this.state = {
       articles : [],
-      loading: false,
-      page:1
+      loading: true,
+      page: 1,
+      totalResults: 0
     }
     document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`;
   }
 
   async updateNews(){
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=510cb7bba4ba41718bcac1108b63fc5a&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.props.setProgress(10);
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({loading: true});
     let data = await fetch(url);
+    this.props.setProgress(30);
     let parseData = await data.json()
+    this.props.setProgress(70);
     this.setState({articles: parseData.articles, 
       totalResults: parseData.totalResults,
-      loading: false})
+      loading: false
+    })
+    this.props.setProgress(100);
   }
 
   async componentDidMount(){
   this.updateNews();
   }
 
-   handlePrevClick = async () =>{
-    console.log("Previous")
-    this.setState({page: this.state.page - 1});
-    this.updateNews();
-  }
-
-   handleNextClick = async () =>{
-    console.log("Next");
-  this.setState({page: this.state.page + 1});
-  this.updateNews();
-  }
+  fetchMoreData = async () => {
+    this.setState({page: this.state.page + 1})
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parseData = await data.json()
+    this.setState({ 
+      articles: this.state.articles.concat(parseData.articles), 
+      totalResults: parseData.totalResults
+    })
+  };
 
   render() {
     return (
-      <div className="container my-3">
+      <>
         <h2 className="text-center" style={{margin: '25px 0px'}}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h2>
         {this.state.loading && <Spinner/>}
+         
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner/>}
+        >
+        <div className="container">
         <div className="row">
-        {!this.state.loading && this.state.articles.map((element)=>{
+        { this.state.articles.map((element)=>{
             return <div className="col-md-4" key={element.url}>
             <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} 
             imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} 
@@ -71,11 +85,9 @@ export class News extends Component {
           </div>
         })} 
         </div>
-        <div className="container d-flex justify-content-between">
-        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
-        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr; </button>
         </div>
-      </div>
+        </InfiniteScroll>
+      </>
     )
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,9 +6,15 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
           <div className="card">
-          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '91%', zIndex: '1'}}>
+            <div style={{display: 'flex',
+                         justifyContent: 'flex-end',
+                         position: 'absolute',
+                         right: '0'
+            }}>
+            <span className="badge rounded-pill bg-danger">
              {source}
              </span>
+            </div>
            <img src={!imageUrl?"https://www.livemint.com/lm-img/img/2024/05/26/1600x900/Cyclone_Remal_1716685044781_1716685045021.jpg":imageUrl} className="card-img-top" alt="..."/>
            <div className="card-body">
            <h5 className="card-title">{title}</h5>
